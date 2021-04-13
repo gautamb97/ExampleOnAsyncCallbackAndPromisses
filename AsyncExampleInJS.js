837c912e56278072a1c7b3408340b1f8df27c340
gautamb97 <www.gautam971997@gmail.com>
2021-04-13 19:10:36 +0530

    Added:Example to show Asyncronous nature of JavaScript

---
+++ AsyncExampleInJS.js
@@ -0,0 +1,8 @@
+console.log("Reading Articles");
+console.log("Cooking Food");
+console.log("Checkong out some updates on news");
+setTimeout(() => {
+    console.log("Hangout with friends....");
+}, 100);
+console.log("Doing Assignment");
+console.log("Playing Games");
